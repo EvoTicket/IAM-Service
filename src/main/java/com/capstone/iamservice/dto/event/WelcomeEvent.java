985f7eaa37f305423c2ef190ff0282892d3ef889
TGG-Henry <tuanphonglqd@gmail.com>
2025-12-01 12:45:22 +0700

    config redis

---
+++ src/main/java/com/capstone/iamservice/dto/event/WelcomeEvent.java
@@ -0,0 +1,16 @@
+package com.capstone.iamservice.dto.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class WelcomeEvent {
+    String email;
+    String fullName;
+    String username;
+}
